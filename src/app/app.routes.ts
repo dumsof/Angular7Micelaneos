a7e54f91b3a6b536b8ec3dc62f1ad99f77a1206a
Darwin <darwinu34@yahoo.es>
2019-06-18 15:58:24 -0500

    144, rutas hijas en el mismo archivo de router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
+/* children: se agregan las rutas hijas que pueden navegar dentro de la pagina */
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+
 const RUTAS: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
+    {
+        path: 'usuario/:id',
+        component: UsuarioComponent,
+        children: [
+            { path: 'nuevo', component: UsuarioNuevoComponent },
+            { path: 'editar', component: UsuarioEditarComponent },
+            { path: 'detalle', component: UsuarioDetalleComponent },
+            { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
+
+        ]
+    },
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
